56e63f5c1b5883aa979f8ca235d01f0a38b1f4a6
Alan Gutierrez <alan@blogometer.com>
2010-06-03 01:58:23 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/mallardsoft/tuple/partial/TuplePartialProject.java
+++ src/mix/java/com/mallardsoft/tuple/partial/TuplePartialProject.java
@@ -1,26 +1,29 @@
 package com.goodworkalan.prattle.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class TuplePartialProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Tuple Partial.
+ *
+ * @author Alan Gutierrez
+ */
+public class TuplePartialProject implements ProjectModule {
+    /**
+     * Build the project definition for Tuple Partial.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.mallardsoft/tuple-partial/0.1.0"))
-                .main()
-                    .depends()
-                        .artifact(new Artifact("com.mallardsoft/tuple/2.0"))
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .artifact(new Artifact("org.testng/testng/5.10/jdk15"))
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
-                        .end()
+                .produces("com.mallardsoft/tuple-partial/0.1.0")
+                .depends()
+                    .production("com.mallardsoft/tuple/+2.0")
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
